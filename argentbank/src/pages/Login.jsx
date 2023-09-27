5854eb12f519c79cc8b69e6f35bdda693ccaab78
pletine <pierre.letinevez@gmail.com>
2023-09-27 22:58:18 +0400

    Make the change to connect with the inputs of the user

--- argentbank/src/pages/Login.jsx
+++ argentbank/src/pages/Login.jsx
@@ -1,27 +1,36 @@
 import "../styles/main_style.scss";
 
 import React from "react";
+import { useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import fetchData from "../_models/fetchData.js";
 
 export default function Login() {
   const navigate = useNavigate();
-  const values = { email: "tony@stark.com", password: "password123" };
+  const nameRef = useRef(null);
+  const pwdRef = useRef(null);
 
   const login = async (event) => {
     event.preventDefault();
 
-    let response = await fetchData(
-      "http://localhost:3001/api/v1/user/login",
-      values
-    );
-    console.log(response);
+    if (nameRef.current.value !== "" && pwdRef.current.value !== "") {
+      const values = {
+        email: nameRef.current.value,
+        password: pwdRef.current.value,
+      };
 
-    if (response.status === 200) {
-      alert("Login OK");
-      navigate("/user/test");
+      let response = await fetchData(
+        "http://localhost:3001/api/v1/user/login",
+        values
+      );
+
+      if (response.status === 200) {
+        navigate("/user/test");
+      } else {
+        alert("Mauvais identifiants");
+      }
     } else {
-      alert("Login Not OK");
+      alert("Veuillez entrer un nom d'utilisateur et un mot de passe");
     }
   };
 
@@ -33,11 +42,11 @@ export default function Login() {
         <form>
           <div className="input-wrapper">
             <label htmlFor="username">Username</label>
-            <input type="text" id="username" />
+            <input type="text" id="username" ref={nameRef} />
           </div>
           <div className="input-wrapper">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" />
+            <input type="password" id="password" ref={pwdRef} />
           </div>
           <div className="input-remember">
             <input type="checkbox" id="remember-me" />
